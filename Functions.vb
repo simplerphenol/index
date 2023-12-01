d723dcd51706defe4a629b1fc24d218823c71637
Alex Paletty <152611933+simplerphenol@users.noreply.github.com>
2023-12-01 14:51:28 +0300

    Create Functions.vb

---
+++ Functions.vb
@@ -0,0 +1,11 @@
+Module FunctionsModule
+
+    Function Greet(name As String) As String
+        Return "Hello, " & name & "!"
+    End Function
+
+    Function AddNumbers(a As Integer, b As Integer) As Integer
+        Return a + b
+    End Function
+
+End Module
